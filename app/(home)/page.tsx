ff368865f54411d7d672804ed02fea56634bdaba
changeonPark <qkrcksrjs@gmail.com>
2024-02-06 16:20:44 +0900

    ✨ feat: server fetching

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,8 +2,22 @@ export const metadata = {
   title: "Home",
 }
 
-const Page = () => {
-  return <h1>Home</h1>
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
+
+const getMovies = async () => {
+  console.log("call getMovies")
+
+  await new Promise((resolve) => setTimeout(resolve, 5000))
+
+  const response = await fetch(URL)
+  const data = await response.json()
+  return data
 }
 
-export default Page
+const HomePage = async () => {
+  const movies = await getMovies()
+
+  return <h1>{JSON.stringify(movies)}</h1>
+}
+
+export default HomePage
