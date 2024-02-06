fdc9ae7b225fd243908c218bfa6e83b6fab192cd
changeonPark <qkrcksrjs@gmail.com>
2024-02-06 11:17:45 +0900

    ✨ feat: routing

---
+++ app/about-us/company/sales/page.tsx
@@ -0,0 +1,7 @@
+const Page = () => {
+  return (
+    <h1>About Us/Company/Sales</h1>
+  )
+}
+
+export default Page
---
+++ app/about-us/page.tsx
@@ -0,0 +1,12 @@
+import Avatar from "./components/Avatar"
+
+const Page = () => {
+  return (
+    <>
+      <h1>About Us</h1>
+      <Avatar />
+    </>
+  )
+}
+
+export default Page
