92bd6782ce910a0746a328b653e864381bb482ba
changeonPark <qkrcksrjs@gmail.com>
2024-03-21 22:39:55 +0900

    ✨ feat: dynamic metadata

--- src/app/(movies)/movies/[id]/page.tsx
+++ src/app/(movies)/movies/[id]/page.tsx
@@ -1,10 +1,25 @@
+import { getMovieInfo } from "@/entities/movie"
 import { MovieInfo } from "@/features/movie-info"
 import { MovieVideos } from "@/features/movie-videos"
+import type { Metadata } from "next"
 import { Suspense } from "react"
 
-const Page = async ({ params: { id } }: { params: { id: string } }) => {
-  // { params: { id: '1' }, searchParams: { region: 'kr', page: '2' } }
+// https://nextjs.org/docs/app/api-reference/functions/generate-metadata
+export async function generateMetadata({
+  params: { id },
+}: {
+  params: { id: string }
+}): Promise<Metadata> {
+  // Next14 이후부터는 fetch의 결과값을 cache하기 때문에 상관없다
+  const movie = await getMovieInfo(id)
 
+  return {
+    title: movie.title,
+    description: movie.overview,
+  }
+}
+
+const Page = async ({ params: { id } }: { params: { id: string } }) => {
   return (
     <div className="flex flex-col gap-8">
       <Suspense fallback={<h1>Loading movie info</h1>}>
--- src/entities/movie/api/get-movie-videos.ts
+++ src/entities/movie/api/get-movie-videos.ts
@@ -1,10 +1,6 @@
 import { URL } from "../constants"
 
 export const getMovieVideos = async (id: string) => {
-  await new Promise((resolve) => setTimeout(resolve, 3000))
-
-  // throw new Error("somthing error...")
-
   const response = await fetch(`${URL}/${id}/videos`)
 
   return response.json()
